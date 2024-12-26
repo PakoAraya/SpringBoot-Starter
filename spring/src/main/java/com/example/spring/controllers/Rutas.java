31459e566088518a578b6583af39a1bda4e388d9
franarayah <franarayah@gmail.com>
2024-12-26 18:33:04 -0300

    Se estudian RequestParam en Spring

--- spring/src/main/java/com/example/spring/controllers/Rutas.java
+++ spring/src/main/java/com/example/spring/controllers/Rutas.java
@@ -1,6 +1,8 @@
 package com.example.spring.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,16 @@ public class Rutas {
   @GetMapping("/hola")
   public String miPrimeraRuta(){
     return "Hola Mundo desde mi primer Spring Controller";
+  }
+  //Ejemplo de Path Params
+  @GetMapping("/libro/{id}/editorial/{editorial}")
+  public String leerLibro(@PathVariable int id, @PathVariable String editorial){
+    return "Leyendo el libro id: " + id+ " de la editorial: " + editorial;
+  }
+
+  //Ejemplo de Query Params
+  @GetMapping("/libro2/{id}")
+  public String leerLibro2(@PathVariable int id, @RequestParam String editorial, @RequestParam String params){
+    return "Leyendo el libro id: " + id+ " editorial " + editorial + " params: " + params;
   }
 }
