544570fa7f2059f32103c62746746ec4264f5c2f
franarayah <franarayah@gmail.com>
2025-01-03 13:52:14 -0300

    Se crea las primeras rutas con controllers con curso de Codigo Morsa

--- spring/src/main/java/com/example/spring/controllers/Rutas.java
+++ spring/src/main/java/com/example/spring/controllers/Rutas.java
@@ -1,12 +1,15 @@
 package com.example.spring.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class Rutas {
+
+  private final Logger logger = LoggerFactory.getLogger(Rutas.class);
 
   @GetMapping("/hola")
   public String miPrimeraRuta(){
@@ -22,5 +25,15 @@ public class Rutas {
   @GetMapping("/libro2/{id}")
   public String leerLibro2(@PathVariable int id, @RequestParam String editorial, @RequestParam String params){
     return "Leyendo el libro id: " + id+ " editorial " + editorial + " params: " + params;
+  }
+
+  //Vamos a aprender a usar post mapping
+  @PostMapping("/libro")
+  public String guardarLibro(@RequestBody Map<String, Object> libro){
+    libro.keySet().forEach(llave -> {
+      logger.debug("Llave: {" + llave + "} Valor: {" + libro.get(llave) + "}");
+    });
+
+    return "Libro Guardado";
   }
 }
